22842bdd6f21825c4ce7e187b153fcd7db042ea8
Erez <erezur5@gmail.com>
2024-06-09 18:21:38 +0300

    todo-app-react directory added.

--- class_exc/09-06-24/react-app/src/App.jsx
+++ class_exc/09-06-24/react-app/src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const data = [
+  { id: '1', title: 'Learn React', isComplete: false },
+  { id: '2', title: 'Build a Todo App', isComplete: false },
+  { id: '3', title: 'Read JavaScript Documentation', isComplete: true },
+  { id: '4', title: 'Write Unit Tests', isComplete: false },
+  { id: '5', title: 'Implement Context, isComplete: true '},
+  { id: '6', title: 'Create Portfolio Website', isComplete: false },
+  { id: '7', title: 'Learn TypeScript', isComplete: false },
+  { id: '8', title: 'Refactor Codebase', isComplete: true },
+  { id: '9', title: 'Optimize Performance', isComplete: false },
+  { id: '10', title: 'Deploy to Production', isComplete: true }
+]
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState(data)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
     </>
   )
 }
